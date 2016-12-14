ba15a6bde4e3299a7ee94d7b3a282c97012ce232
Johnny Xu <johnnyxl26@gmail.com>
2016-12-13 23:26:28 -0600

    Capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-
+server '54.218.32.43', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
